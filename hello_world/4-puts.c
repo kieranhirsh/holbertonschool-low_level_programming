02327ee406e3cd8dd41da0d5bceeea761da96659
kieranhirsh <kieranhirsh@gmail.com>
2024-02-19 01:05:22 +0000

    C - Hello, World: task 4. Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints "\"Programming is like building a multilingual
+ * puzzle", followed by a new line.
+ */
+void main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
+
