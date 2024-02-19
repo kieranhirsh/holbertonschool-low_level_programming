5a4d2e2b6d261a23aa4b62f4998d08256a15b913
kieranhirsh <kieranhirsh@gmail.com>
2024-02-19 01:23:55 +0000

    C - Hello, World: task 5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * prints "with proper grammar, but the outcome is a piece of art,",
+ * followed by a new line.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+	return (0);
+}
+
