896151ecc3c88418b6bc365a56367dd7a7b161e8
kieranhirsh <kieranhirsh@gmail.com>
2024-02-19 03:50:36 +0000

    C - Variables, if, else, while: task 0. Positive anything is better than negative nothing

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * this program tells you whether a random number is +ve or -ve
+ *
+ * Return: 0 (always success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+
+	return (0);
+}
