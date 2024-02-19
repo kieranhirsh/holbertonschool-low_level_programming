fdc9477fdc3bcf00895eac5bbfb2df70969bcf0c
kieranhirsh <kieranhirsh@gmail.com>
2024-02-19 02:50:18 +0000

    C - Hello, World: task 6. Size is not grandeur, and territory does not make a nation, attempt 2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n
-			Size of an int: 4 byte(s)\n
-			Size of a long int: 4 byte(s)\n
-			Size of a long long int: 8 byte(s)\n
-			Size of a float: 4 byte(s)\n");
+	char my_str = "Size of a char: 1 byte(s)\n\
+		       Size of an int: 4 byte(s)\n\
+		       Size of a long int: 4 byte(s)\n\
+		       Size of a long long int: 8 byte(s)\n\
+		       Size of a float: 4 byte(s)\n";
+	
+	printf(my_str);
 
 	return (0);
 }
