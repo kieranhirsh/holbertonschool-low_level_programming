778f9e0ccd974b33cd19c0a5aa8b9ca8892ae445
kieranhirsh <kieranhirsh@gmail.com>
2024-02-19 02:41:50 +0000

    C - Hello, World: task 6. Size is not grandeur, and territory does not make a nation

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n
+			Size of an int: 4 byte(s)\n
+			Size of a long int: 4 byte(s)\n
+			Size of a long long int: 8 byte(s)\n
+			Size of a float: 4 byte(s)\n");
+
+	return (0);
+}
+
