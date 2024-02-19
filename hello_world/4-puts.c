cacaa612ea8cbd2d16e5767e2538c27c347c1b39
kieranhirsh <kieranhirsh@gmail.com>
2024-02-19 01:16:35 +0000

    C - Hello, World: task 4. Hello, puts. making betty (documentation) happy

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- * main - this program prints "\"Programming is like building a multilingual
+ * main - entry point.
+ * prints "\"Programming is like building a multilingual
  * puzzle", followed by a new line.
+ *
+ * Return: always 0 (success)
  */
 int main(void)
 {
