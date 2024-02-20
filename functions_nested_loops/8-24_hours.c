35da86478deb8dbfa47136f8ed60b90e975a7bb4
kieranhirsh <kieranhirsh@gmail.com>
2024-02-20 04:48:17 +0000

    C - Functions, nested loops: task 8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints all the minutes in a day
+ */
+void jack_bauer(void)
+{
+	char hh;
+	char h;
+	char mm;
+	char m;
+
+	for (hh = '0' ; hh <= '2' ; hh++)
+	{
+		for (h = '0' ; h <= '4' ; h++)
+		{
+			for (mm = '0' ; mm <= '5' ; mm++)
+			{
+				for (m = '0' ; m <= '9' ; m++)
+				{
+					_putchar(hh);
+					_putchar(h);
+					_putchar(':');
+					_putchar(mm);
+					_putchar(m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
