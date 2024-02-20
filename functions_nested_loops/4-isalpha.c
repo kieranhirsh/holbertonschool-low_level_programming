8d0825470e15d7477e9d0d397277ccd876c1b00a
kieranhirsh <kieranhirsh@gmail.com>
2024-02-20 03:24:45 +0000

    C - Functions, nested loops: task 4. isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether a given character is alphabetic
+ * @c: the character to be checked
+ *
+ * Return: 1 if @c is lowercase
+ *         0 otherwise
+ *
+ */
+int _isalpha(int c)
+{
+	if (c <= 122 && c >= 65)
+	{
+		if (c <= 96 && c >= 91)
+			return (0);
+		else
+			return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
