69513408264880e54c90b774d366b159a2c1a83a
kieranhirsh <kieranhirsh@gmail.com>
2024-02-20 02:46:17 +0000

    C - Functions, nested loops: task 2. 10 x alphabet. the checker sucks

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * prints the alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ii;
+
+	for (ii=1 ; ii<=10 ; ii++)
+	{
+		print_alphabet();
+	}
+
+	return (0);
+}
