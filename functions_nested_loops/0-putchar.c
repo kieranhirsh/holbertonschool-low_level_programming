fee2e24dc306ea562ecb4e46f727ba682fa7b9d8
kieranhirsh <kieranhirsh@gmail.com>
2024-02-20 00:20:35 +0000

    C - Functions, nested loops: task 0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * this program prints "_putchar"
+ *
+ * Return: 0 (always success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
+
