6caacf3209573b2c21540faff8921106e5744f04
kieranhirsh <kieranhirsh@gmail.com>
2024-02-23 01:11:29 +0000

    C - Functions, nested loops: task 11. 98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * tprint_to_98 - prints all the integer numbers between a given integer and 98
+ * @n: the input number
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			if (n >= 100)
+			{
+				_putchar((n / 100) + '0');
+				_putchar(((n / 10) % 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+			n = n - 1;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n < 98)
+		{
+			if (n < 1)
+			{
+				_putchar('-');
+				if (n < -9)
+				{
+					_putchar((-1) * (n / 10) + '0');
+				}
+				_putchar((-1) * (n % 10) + '0');
+			}
+			else if (n < 10)
+			{
+				_putchar((n % 10) + '0');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+			n = n + 1;
+		}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
+
