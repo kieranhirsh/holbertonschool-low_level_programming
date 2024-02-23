8d1e766a7ac74c0095f5805467c1b941ea8960b6
kieranhirsh <kieranhirsh@gmail.com>
2024-02-23 03:56:14 +0000

    C - Functions, nested loops: task 16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *        prints the first 50 numbers of the fibonacci sequence
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ii;
+	long int old, new, newnew;
+	long int oldb, newb, newnewb;
+
+	old = 1;
+	new = 2;
+	oldb = 0;
+	newb = 0;
+
+	printf("%ld", old);
+	printf(", %ld", new);
+
+	for (ii = 3 ; ii <= 100 ; ii++)
+	{
+		printf(", ");
+		newnew = old + new;
+		if (newnew >= 1000000000 || newnewb > 0)
+		{
+			newnewb = oldb + newb + ((newnew - (newnew % 1000000000)) / 1000000000);
+			newnew = newnew % 1000000000;
+			printf("%ld", newnewb);
+			printf("%9.9ld", newnew);
+			oldb = newb;
+			newb = newnewb;
+		}
+		else
+		{
+			printf("%ld", newnew);
+		}
+		old = new;
+		new = newnew;
+	}
+	printf("\n");
+
+	return (0);
+}
+
