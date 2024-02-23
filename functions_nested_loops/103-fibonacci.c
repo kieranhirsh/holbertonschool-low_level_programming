15f147add797ab3721de98dec28e49b442435c30
kieranhirsh <kieranhirsh@gmail.com>
2024-02-23 03:13:11 +0000

    C - Functions, nested loops: task 15. Even Liber Abbaci

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *        prints the sum of the even fibonacci numbers less than 4,000,000
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ii;
+	long int old, new, newnew, total;
+
+	old = 1;
+	new = 2;
+	newnew = 0;
+	total = 2;
+
+	while (newnew <= 4000000)
+	{
+		newnew = old + new;
+		if (newnew % 2 == 0)
+			total = total + newnew;
+		old = new;
+		new = newnew;
+	}
+	printf("%ld\n", total);
+
+	return (0);
+}
+
