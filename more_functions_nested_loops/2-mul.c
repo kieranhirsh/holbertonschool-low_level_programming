928f48cd5e0bdc4638ea233629202a259a3527d4
kieranhirsh <kieranhirsh@gmail.com>
2024-02-25 23:41:34 +0000

    C - More functions, more nested loops. Task 2. Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+ * _mul - multiplies two given numbers
+ * @a: the first given number
+ * @b: the second given number
+ *
+ * Return: @c: equal to @a * @b
+ *
+ */
+int mul(int a, int b)
+{
+	int c;
+	
+	c = a * b;
+
+	return (c);
+}
+
