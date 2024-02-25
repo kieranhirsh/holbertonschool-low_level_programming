84ed0757c2c0b034bfd56416574cfeee748de5fc
kieranhirsh <kieranhirsh@gmail.com>
2024-02-25 23:32:51 +0000

    C - More functions, more nested loops. Task 0. isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ * _isupper - checks whether a given character is uppercase
+ * @c: the character to be checked
+ *
+ * Return: 1 if @c is uppercase
+ *         0 otherwise
+ *
+ */
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+		return (1);
+	else
+		return (0);
+}
+
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_NESTED_LOOPS
+#define FUNCTIONS_NESTED_LOOPS
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
