b49db6d4717e5c5179113a2c663eb6363ced93ed
kieranhirsh <kieranhirsh@gmail.com>
2024-02-25 23:59:31 +0000

    C - More functions, more nested loops. Task 4. I believe in numbers and signs

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers zero to nine, excluding two and four
+ *
+ */
+void print_most_numbers(void)
+{
+	int ii;
+
+	for (ii = 0 ; ii <= 9 ; ii++)
+	{
+		if (ii == 2 | ii == 4)
+			continue
+		_putchar(ii + '0');
+	}
+	_putchar('\n');
+}
+
