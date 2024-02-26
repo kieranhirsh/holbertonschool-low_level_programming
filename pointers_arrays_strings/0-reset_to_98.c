76b9abc27b5c8ec516a5bee0bebc923951660680
kieranhirsh <kieranhirsh@gmail.com>
2024-02-26 04:49:48 +0000

    C - Pointers, arrays and strings. Task 0. 98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - takes a pointer and updates the value it points to to 98
+ * @n: the input pointer
+ *
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MORE_FUNCTIONS_MORE_NESTED_LOOPS
+#define MORE_FUNCTIONS_MORE_NESTED_LOOPS
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
