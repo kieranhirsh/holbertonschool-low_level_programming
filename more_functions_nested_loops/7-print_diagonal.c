24b163699969dc1129b6302e835d1dc71f9a9f70
kieranhirsh <kieranhirsh@gmail.com>
2024-02-26 00:40:13 +0000

    C - More functions, more nested loops. Task 7. I feel like I am diagonally parked in a parallel universe

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line of length @n
+ * @n: the length of the line to be printed
+ *
+ */
+void print_diagonal(int n)
+{
+	int ii, jj;
+
+	if (n > 0)
+	{
+		for (ii = 1 ; ii <= n ; ii++)
+		{
+			for (jj = 1; jj < ii ; jj ++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
