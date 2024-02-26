5fec9a52512a1d545cd9901c3d3804e1ec051696
kieranhirsh <kieranhirsh@gmail.com>
2024-02-26 00:00:55 +0000

    C - More functions, more nested loops. Task 4. I believe in numbers and signs. bug fixing

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,8 @@ void print_most_numbers(void)
 
 	for (ii = 0 ; ii <= 9 ; ii++)
 	{
-		if (ii == 2 | ii == 4)
-			continue
+		if (ii == 2 || ii == 4)
+			continue;
 		_putchar(ii + '0');
 	}
 	_putchar('\n');
