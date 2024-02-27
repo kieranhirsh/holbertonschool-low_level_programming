d7b496539bc5d650db7ec1007becf28b3540e643
kieranhirsh <kieranhirsh@gmail.com>
2024-02-27 03:42:05 +0000

    C - Pointers, arrays and strings. Task 8. Arrays are not pointers. might fail if n > len(a)

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints a specified number of elements on an array
+ * @a: the given array
+ * @n: the number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int ii;
+
+	for (ii = 0 ; ii < n ; ii++)
+	{
+		printf("%d",a[ii]);
+		if (ii != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
+
