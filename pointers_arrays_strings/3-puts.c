d168df13396fafe0c8e40475d57a7b77fa2e9c3e
kieranhirsh <kieranhirsh@gmail.com>
2024-02-27 00:04:09 +0000

    C - Pointers, arrays and strings. Task 3. I do not fear computers. I fear the lack of them. this should work, but it's so inelegant to include _strlen like this when it could just be compiled with the rest of the code

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -11,3 +11,23 @@ void _puts(char *str)
 	write(1, "\n", 1);
 }
 
+/**
+ * _strlen - calculates the length of a given string
+ * @s: the address of the given string
+ *
+ * Return: the length of the given string
+ */
+int _strlen(char *s)
+{
+	int i = 0, n = -2;
+
+	while (n != (i - 1))
+	{
+		if (s[i] == '\0')
+			n = i;
+		i += 1;
+	}
+
+	return (n);
+}
+
