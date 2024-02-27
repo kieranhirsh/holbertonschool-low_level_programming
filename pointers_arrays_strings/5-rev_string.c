a86b8d3fb8fc36e7d1f5987f917a088b0267b7e4
kieranhirsh <kieranhirsh@gmail.com>
2024-02-27 00:55:28 +0000

    C - Pointers, arrays and strings. Task 5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * rev°string - reverses a string
+ * @s: the given string
+ */
+void rev_string(char *s)
+{
+	char rts[10];
+	int i;
+
+	for (i = 0 ; i < _strlen(s) ; i++)
+		rts[i] = s[i];
+	for (i = 0 ; i < _strlen(s) ; i++)
+		s[i] = rts[_strlen(s) - i - 1];
+}
+
+/**
+ * _strlen - calculates the length of a given string
+ * @s: the address of the given string
+ *
+ * Return: the length of the given string
+ */
+int _strlen(char *s)
+{
+	int i = 0, n = -2;
+
+	while (n != (i - 1))
+	{
+		if (s[i] == '\0')
+			n = i;
+		i += 1;
+	}
+
+	return (n);
+}
+
