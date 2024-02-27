0eb3024ebadd9a4ff3069ee682c854f3c3a1d6e6
kieranhirsh <kieranhirsh@gmail.com>
2024-02-27 04:13:54 +0000

    C - Pointers, arrays and strings. Task 9. strcpy. using _strlen instead of strlen

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * print_array - prints a specified number of elements on an array
- * @a: the given array
- * @n: the number of elements to print
+ * _strcpy - copies a string to another string
+ * @dest: the string to be copied into
+ * @src: the string to copy
+ *
+ * Return: the pointer to @dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int ii, len;
 
-	len = strlen(src);
+	len = _strlen(src);
 
 	for (ii = 0 ; ii < len; ii++)
 		dest[ii] = src[ii];
 
 	return (&dest[0]);
+}
+
+/**
+ * _strlen - calculates the length of a given string
+ * @s: the address of the given string
+ *
+ * Return: the length of the given string
+ */
+int _strlen(char *s)
+{
+	int i = 0, n = -2;
+
+	while (n != (i - 1))
+	{
+		if (s[i] == '\0')
+			n = i;
+		i += 1;
+	}
+
+	return (n);
 }
 
