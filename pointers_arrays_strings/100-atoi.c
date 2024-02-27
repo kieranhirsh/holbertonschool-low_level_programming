d20cd5c0b76bd9ecdf69ca185dd6d484e38e5c2a
kieranhirsh <kieranhirsh@gmail.com>
2024-02-27 04:49:41 +0000

    C - Pointers, arrays and strings. Task 10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+/**
+ * _atoi - converts a string to an integer
+ *         only considers the first number in the string
+ *         considers every '-' before the first number
+ * @s: the string to be converted
+ *
+ * Return: @num: the int
+ */
+int _atoi(char *s)
+{
+	int ii = 0, num = 0;
+	int numstart = -1, numend = -1;
+	int mult = 1;
+
+	while (numstart < 0)
+	{
+		if (s[ii] >= 48 && s[ii] <= 57)
+			numstart = ii;
+		else
+			ii += 1;
+	}
+	while (numend < 0)
+	{
+		if (s[ii] < 48 || s[ii] > 57)
+			numend = ii - 1;
+		else
+			ii += 1;
+	}
+	for (ii = numend ; ii >= numstart ; ii--)
+	{
+		num += (s[ii] - 48) * mult;
+		mult *= 10;
+	}
+	for (ii = numstart - 1 ; ii >= 0 ; ii--)
+	{
+		if (s[ii] == 45)
+			num = num * (-1);
+	}
+
+	return (num);
+}
+
