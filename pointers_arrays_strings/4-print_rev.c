bf82d1a34fdc82e2c3177b43164f1e0b84fbef79
kieranhirsh <kieranhirsh@gmail.com>
2024-02-27 00:09:57 +0000

    C - Pointers, arrays and strings. Task 4. I can only go one way. I've not got a reverse gear

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: the given string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) ; i >= 0 ; i--)
+		write(1, s[i], 1);
+	write(1, "\n", 1);
+}
+
+/**
+ * _strlen - calculates the length of a given string
+ * @s: the address of the given string
+ *
+ * Return: the length of the given string
+ */
+int _strlen(char *s)
+{
+	int i = 0, n = -2;
+
+	while (n != (i - 1))
+	{
+		if (s[i] == '\0')
+			n = i;
+		i += 1;
+	}
+
+	return (n);
+}
+
