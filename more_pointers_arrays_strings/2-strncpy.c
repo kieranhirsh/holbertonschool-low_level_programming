94dbcf12aa22e9b1393e4e58f941e3c560faad21
kieranhirsh <kieranhirsh@gmail.com>
2024-02-28 03:47:19 +0000

    C - More pointers, arrays and strings. Task 2. strncpy. trying to fix the case of n > len(src)

--- more_pointers_arrays_strings/2-strncpy.c
+++ more_pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,30 @@
 /**
- * _strncat - concatenates two strings
- * @dest: the string to be appended on to
- * @src: the string to append
- * @n: the number of characters of @src to append to @dest
+ * _strncpy - copies one string onto another
+ * @dest: the string to be copied onto
+ * @src: the string to copy
+ * @n: the number of characters of @src to copy to @dest
  *
  * Return: a pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int counts = 0;
-	int ii, min;
+	int ii;
 
 	while (src[counts] != '\0')
 		counts = counts + 1;
-	if (n < counts)
-		min = n - 1;
+	if ((n - 1) <= counts)
+	{
+		for (ii = 0 ; ii <= n - 1 ; ii++)
+			dest[ii] = src[ii];
+	}
 	else
-		min = counts;
-
-	for (ii = 0 ; ii <= min ; ii++)
-		dest[ii] = src[ii];
+	{
+		for (ii = 0 ; ii <= counts ; ii++)
+			dest[ii] = src[ii];
+		for (ii = counts + 1 ; ii <= n - 1 ; ii++)
+			dest[ii] = '\n';
+	}
 
 	return (dest);
 }
