86265b28a30f7e60555258769b407418e0e8cad5
kieranhirsh <kieranhirsh@gmail.com>
2024-02-28 00:52:07 +0000

    C - More pointers, arrays and strings. Task 1. strncat

---
+++ more_pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+/**
+ * _strncat - concatenates two strings
+ * @dest: the string to be appended on to
+ * @src: the string to append
+ * @n: the number of characters of @src to append to @dest
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int countd = 0, counts = 0;
+	int ii;
+
+	while (dest[countd] != '\0')
+		countd = countd + 1;
+	while (dest[counts] != '\0' && counts < n)
+		counts = counts + 1;
+
+	for (ii = countd ; ii <= countd + counts ; ii++)
+		dest[ii] = src[ii - countd];
+
+	return (dest);
+}
+
--- more_pointers_arrays_strings/main.h
+++ more_pointers_arrays_strings/main.h
@@ -2,5 +2,7 @@
 #define MORE_FUNCTIONS_MORE_NESTED_LOOPS
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
 
 #endif
