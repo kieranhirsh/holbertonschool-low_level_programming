80feb0b00e28ced5786cb14a7ff1e1c5ec0a5e18
kieranhirsh <kieranhirsh@gmail.com>
2024-02-28 05:10:24 +0000

    C - More pointers, arrays and strings. Task 6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ more_pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+/**
+ * cap_string - capitalised all the words of a string
+ * @s: a string
+ *
+ * Return: the newly uppercased string
+ */
+char *cap_string(char *s)
+{
+	int ii = 0, next = 0;
+
+	while (s[ii] != '\0')
+	{
+		if (next == 1)
+		{
+			if (s[ii] >= 97 && s[ii] <= 122)
+				s[ii] = s[ii] - 32;
+			next = 0;
+		}
+		if (s[ii] == ' ')
+			next = 1;
+		if (s[ii] == '\t')
+			next = 1;
+		if (s[ii] == '\n')
+			next = 1;
+		if (s[ii] == ',')
+			next = 1;
+		if (s[ii] == ';')
+			next = 1;
+		if (s[ii] == '.')
+			next = 1;
+		if (s[ii] == '!')
+			next = 1;
+		if (s[ii] == '?')
+			next = 1;
+		if (s[ii] == '"')
+			next = 1;
+		if (s[ii] == '(')
+			next = 1;
+		if (s[ii] == ')')
+			next = 1;
+		if (s[ii] == '{')
+			next = 1;
+		if (s[ii] == '}')
+			next = 1;
+		ii = ii + 1;
+	}
+
+	return (s);
+}
+
