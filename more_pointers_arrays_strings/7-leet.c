4da2c0180b3d8bd9856479d5ba8a629cf1c0e44c
kieranhirsh <kieranhirsh@gmail.com>
2024-02-29 02:05:37 +0000

    C - More pointers, arrays and strings. Task 7. Mozart composed his music not for the elite, but for everybody. the checker wants only 2 loops

--- more_pointers_arrays_strings/7-leet.c
+++ more_pointers_arrays_strings/7-leet.c
@@ -6,17 +6,14 @@
  */
 char *leet(char *s)
 {
-	char alpha[] = "aeotl";
-	char leet[] = "43071";
-	int ii = 0, jj = 0;
+	char alpha[6] = "aeotl";
+	char leet[6] = "43071";
+	int ii = 0;
 	int kk;
-
-	while (alpha[jj] != '\0')
-		jj = jj + 1;
 
 	while (s[ii] != '\0')
 	{
-		for (kk = 0 ; kk < jj ; kk++)
+		for (kk = 0 ; kk <= 4 ; kk++)
 		{
 			if (s[ii] == alpha[kk] || s[ii] == alpha[kk] - 32)
 				s[ii] = leet[kk];
