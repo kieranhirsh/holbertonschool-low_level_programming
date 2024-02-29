e587cb73ba29ca6e28fe26cb4c3e06fdcbdf56d7
kieranhirsh <kieranhirsh@gmail.com>
2024-02-29 01:58:25 +0000

    C - More pointers, arrays and strings. Task 6. Expect the best. Prepare for the worst. Capitalize on what comes. I couldn't, in good conscience, leave that meany ifs in my code

--- more_pointers_arrays_strings/6-cap_string.c
+++ more_pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,13 @@
  */
 char *cap_string(char *s)
 {
-	int ii = 0, next = 1;
+	char seps[] = " \t\n,;.!?\"(){}";
+	int ii = 0, jj = 0;
+	int next = 1;
+	int kk;
+
+	while (seps[jj] != '\0')
+		jj = jj + 1;
 
 	while (s[ii] != '\0')
 	{
@@ -16,32 +22,11 @@ char *cap_string(char *s)
 				s[ii] = s[ii] - 32;
 			next = 0;
 		}
-		if (s[ii] == ' ')
-			next = 1;
-		if (s[ii] == '\t')
-			next = 1;
-		if (s[ii] == '\n')
-			next = 1;
-		if (s[ii] == ',')
-			next = 1;
-		if (s[ii] == ';')
-			next = 1;
-		if (s[ii] == '.')
-			next = 1;
-		if (s[ii] == '!')
-			next = 1;
-		if (s[ii] == '?')
-			next = 1;
-		if (s[ii] == '"')
-			next = 1;
-		if (s[ii] == '(')
-			next = 1;
-		if (s[ii] == ')')
-			next = 1;
-		if (s[ii] == '{')
-			next = 1;
-		if (s[ii] == '}')
-			next = 1;
+		for (kk = 0 ; kk < jj ; kk++)
+		{
+			if (s[ii] == seps[kk])
+				next = 1;
+		}
 		ii = ii + 1;
 	}
 
