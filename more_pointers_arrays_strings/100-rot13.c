db33b87c50609827161c7b2cac4d7d241bf6de37
kieranhirsh <kieranhirsh@gmail.com>
2024-02-29 02:36:31 +0000

    C - More pointers, arrays and strings. Task 8. rot13

---
+++ more_pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+/**
+ * rot13 - encodes a string using rot13
+ * @s: a string
+ *
+ * Return: the newly encoded string
+ */
+char *rot13(char *s)
+{
+	char alpha[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int ii = 0;
+	int kk;
+
+	while (s[ii] != '\0')
+	{
+		for (kk = 0 ; kk <= 4 ; kk++)
+		{
+			if (s[ii] == alpha[kk])
+				s[ii] = rot[kk];
+		}
+		ii = ii + 1;
+	}
+
+	return (s);
+}
+
