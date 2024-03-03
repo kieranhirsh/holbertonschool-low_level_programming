9e8d4b2f7961b4570266d49e5f73c62061b0d635
kieranhirsh <kieranhirsh@gmail.com>
2024-03-03 23:57:55 +0000

    C - Even more pointers, arrays and strings. Task 0. memset

---
+++ even_more_pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * memset - fills memory with a constant byte
+ * @s: the memory area
+ * @b: the byte to fill
+ * @n: the number of byte to fill
+ *
+ * Return: pointer to the memory area @s
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int ii;
+
+	for (ii = 0 ; ii < n ; ii++)
+		s[ii] = b;
+
+	return (s);
+}
+
---
+++ even_more_pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef EVEN_MORE_FUNCTIONS_MORE_NESTED_LOOPS
+#define EVEN_MORE_FUNCTIONS_MORE_NESTED_LOOPS
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
