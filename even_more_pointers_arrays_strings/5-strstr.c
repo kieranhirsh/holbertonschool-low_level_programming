3a5d30c1cae2489f92e2677a0f505488e1207b0a
kieranhirsh <kieranhirsh@gmail.com>
2024-03-04 23:45:57 +0000

    C - Even more pointers, arrays and strings. Task 5. strstr

---
+++ even_more_pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+/**
+ * _strstr - searches a string for a substring
+ * @haystack: the given string
+ * @needle: the given substring
+ *
+ * Return: the pointer to the first byte in @haystack that matches @needle
+ *         NULL, if @needle not found
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int ii = 0;
+	unsigned int jj;
+
+	while (haystack[ii] != '\0')
+	{
+		if (haystack[ii] == needle[0])
+		{
+			jj = 1;
+			while (needle[jj] != '\0')
+			{
+				if (haystack[ii + jj] != needle[jj])
+					break;
+				jj += 1;
+			}
+			if (needle[jj] == '\0')
+				return (&haystack[ii]);
+		}
+		ii += 1;
+	}
+
+	return ('\0');
+}
+
