a5d46bebeec10964a7505e07e729055b1c06f5d6
kieranhirsh <kieranhirsh@gmail.com>
2024-03-04 00:36:22 +0000

    C - Even more pointers, arrays and strings. Task 2. strchr

---
+++ even_more_pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+/**
+ * _strchr - finds the first instance of a character in a string
+ * @s: the given string
+ * @c: the given character
+ *
+ * Return: pointer to the first occurence of @c in @s, or NULL if not found
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int ii = 0;
+
+	while (s[ii] != c && s[ii] != '\0')
+	{
+		ii += 1;
+	}
+
+	return (&s[ii]);
+}
+
