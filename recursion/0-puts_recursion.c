76261f130efb495d663b5af91e14d63af7b1b441
kieranhirsh <kieranhirsh@gmail.com>
2024-03-05 06:12:22 +0000

    C - Recursion. Task 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget. no longer printing NULL at end of string

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,10 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(s[0]);
-	if (s[0] != '\0')
-		_puts_recursion(s + 1);
-	else
+	if (s[0] == '\0')
+	{
 		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 }
 
