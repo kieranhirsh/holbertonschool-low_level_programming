8f09fbff8c4a34b9f6dec0e5d7045d93a8885b61
kieranhirsh <kieranhirsh@gmail.com>
2024-03-05 12:20:28 +0000

    C - Recursion. Task 3. You mustn't be afraid to dream a little bigger, darling

---
+++ recursion/3-factorial.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial of a number
+ * @n: the given number
+ *
+ * Return: @fact - the factorial of @n
+ *
+ */
+int factorial(int n)
+{
+	int fact;
+
+	if (n < 0)
+	{
+		fact = -1;
+	}
+	else if (n == 0)
+	{
+		fact = 1;
+	}
+	else
+	{
+		fact = n;
+		fact *= factorial(n - 1);
+	}
+
+	return (fact);
+}
+
