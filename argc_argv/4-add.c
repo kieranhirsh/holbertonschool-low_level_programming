b1024219e9be25d7160fa9c835956718a5ebb76d
kieranhirsh <kieranhirsh@gmail.com>
2024-03-06 04:10:32 +0000

    C - argc, argv. Task 4. To infinity and beyond

---
+++ argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: the number of command line arguments passed to the program
+ * @argv: the command line arguments passed to the program
+ *
+ * Return: 0 if sum works
+ *         1 otherwise
+ *
+ */
+int main(int argc, char **argv)
+{
+	int ii;
+	int sum = 0;
+
+	for (ii = 1 ; ii < argc ; ii++)
+	{
+		if ((atoi(argv[ii]) == 0) && (argv[ii] != '0'))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[ii];
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
