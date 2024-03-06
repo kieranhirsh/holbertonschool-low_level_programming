5d2e5750dc0dc802906cd11b17980d1d6c4b90a0
kieranhirsh <kieranhirsh@gmail.com>
2024-03-06 03:48:31 +0000

    C - argc, argv. Task 0. It ain't what they call you, it's what you answer to. making betty happy

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,6 +2,8 @@
 
 /**
  * main - check the code
+ * @argc: the number of command line arguments passed to the program (unused)
+ * @argv: the command line arguments passed to the program
  *
  * Return: Always 0.
  */
