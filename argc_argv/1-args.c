88495bf28b2939a96bb559c9c7c3508e1c90366d
kieranhirsh <kieranhirsh@gmail.com>
2024-03-06 03:49:35 +0000

    C - argc, argv. Task 1. Silence is argument carried out by other means

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: the number of command line arguments passed to the program
+ * @argv: the command line arguments passed to the program (unused)
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
