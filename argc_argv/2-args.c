5f3402078e276b69fb383540e77115bb5447842b
kieranhirsh <kieranhirsh@gmail.com>
2024-03-06 03:53:10 +0000

    C - argc, argv. Task 2. The best argument against democracy is a five-minute conversation with the average voter

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: the number of command line arguments passed to the program
+ * @argv: the command line arguments passed to the program
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	unsigned int ii;
+
+	for (ii = 1 ; ii < argc ; ii++)
+		printf("%s\n", argv[ii]);
+
+	return (0);
+}
