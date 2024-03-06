58ccb493ab8b9f2c3fa5679b768a8bb547150bc0
kieranhirsh <kieranhirsh@gmail.com>
2024-03-06 00:34:01 +0000

    C - Recursion. Task 6. Inception. Is it possible?. fixing the n = 2 case

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -31,10 +31,10 @@ int is_prime_number(int n)
 {
 	int x = 3;
 
-	if ((n < 2) || (n % 2 == 0))
-		return (0);
-	else if (n == 2)
+	if (n == 2)
 		return (1);
+	else if ((n < 2) || (n % 2 == 0))
+		return (0);
 	else
 		return (prime_checker(n, x));
 }
