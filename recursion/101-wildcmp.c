c464fcbf027bdbd1f5cc33c2e3cfe7c73d9011cc
kieranhirsh <kieranhirsh@gmail.com>
2024-03-07 14:12:20 +0000

    C - Recursion. Task 8. Inception. Now, before you bother telling me it's impossible.... trying to fit it all in one function, using a very clever if to step along both branches when s2 = *

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,31 +1,3 @@
-#include "main.h"
-
-/**
- * wildcase - compares two strings
- * @s1: a string to be compared
- * @s2: another string to be compared
- *
- * Return: 1 if strings can be considered identical
- *         0 if not
- *         -1 if an error has occured
- *
- */
-int wildcase(char *s1, char *s2)
-{
-	if (s2[0] == '*')
-		return (wildcase(s1, s2 + 1));
-	else if (s2[0] == '\0')
-		return (1);
-	else if (s1[0] == '\0')
-		return (0);
-	else if (s1[0] != s2[0])
-		return (wildcase(s1 + 1, s2));
-	else if (s1[0] == s2[0])
-		return (wildcmp(s1 + 1, s2 + 1));
-	else
-		return (-1);
-}
-
 /**
  * wildcmp - compares two strings
  * @s1: a string to be compared
@@ -33,18 +5,37 @@ int wildcase(char *s1, char *s2)
  *
  * Return: 1 if strings can be considered identical
  *         0 if not
- *         -1 if an error has occured due to the special character '*'
+ *         -1 if an impossible logic path has been taken
  *
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (s2[0] == '*')
-		return (wildcase(s1, s2 + 1));
-	else if (s1[0] != s2[0])
-		return (0);
-	else if ((s1[0] == '\0') && (s2[0] == '\0'))
-		return (1);
+	if (s1[0] == '\0')
+	{
+		if (s2[0] == '\0')
+			return (1);
+		else if (s2[0] == '*')
+			return (wildcmp(s1, s2 + 1));
+		else
+			return (0);
+	}
 	else
-		return (wildcmp(s1 + 1, s2 + 1));
+	{
+		if (s1[0] == s2[0])
+		{
+			return (wildcmp(s1 + 1, s2 + 1));
+		}
+		else if (s2[0] == '*')
+		{
+			if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+				return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (-1);
 }
 
