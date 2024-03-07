67b8d4712dca68e252194293bf26910bc34dbb48
kieranhirsh <kieranhirsh@gmail.com>
2024-03-07 00:20:30 +0000

    C - Even more pointers, arrays and strings. Task 8. Double pointer, double fun. making betty happy

--- even_more_pointers_arrays_strings/100-set_string.c
+++ even_more_pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * set-string - sets the value of a pointer to a char
+ * set_string - sets the value of a pointer to a char
  * @s: the pointer
- * @b: the char
+ * @to: the char
  *
  */
 void set_string(char **s, char *to)
