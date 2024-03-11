d833208f3b0db1216c6f5cda85c658013a91ac09
kieranhirsh <kieranhirsh@gmail.com>
2024-03-11 23:52:54 +0000

    C - malloc, free. Task 0. Float like a butterfly, sting like a bee. making betty happy

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars, and initializes it
  * @size: the size of the array to be created
  * @c: the char to be used for initialisation
  *
