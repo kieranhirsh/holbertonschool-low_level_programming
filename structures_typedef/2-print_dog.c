9ade69197930ad261705c5b46b6a990f66ff23e4
kieranhirsh <kieranhirsh@gmail.com>
2024-03-12 12:51:09 +0000

    C - Structures, typedef. Task 2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a dog
+ * @d: the dog
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: ");
+		if ((*d).name)
+			printf("%s\n", (*d).name);
+		else
+			printf("(nil)\n");
+		printf("Age: ");
+		if ((*d).age)
+			printf("%9.6f\n", (*d).age);
+		else
+			printf("(nil)\n");
+		printf("Owner: ");
+		if ((*d).owner)
+			printf("%s\n", (*d).owner);
+		else
+			printf("(nil)\n");
+	}
+}
+
