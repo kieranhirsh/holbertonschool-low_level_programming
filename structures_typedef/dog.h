027dd50ca9620f2dff779b772072022be61c7a4b
kieranhirsh <kieranhirsh@gmail.com>
2024-03-12 12:24:29 +0000

    C - Structures, typedef. Task 0. Poppy. Making betty happy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - defines a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: this struct is for the "C - Structures, typedef" project
+ */
 struct dog
 {
 	char *name;
