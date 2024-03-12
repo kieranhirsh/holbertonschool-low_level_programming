ac022fca63b376faf827edfc8259e0e089bc112d
kieranhirsh <kieranhirsh@gmail.com>
2024-03-12 12:31:18 +0000

    C - Structures, typedef. Task 1. A dog is the only thing on earth that loves you more than you love yourself

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialises a dog
+ * @d: the struct of type dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
+
