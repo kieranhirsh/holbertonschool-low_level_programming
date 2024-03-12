5fafad887e13d6af0fd4b743fd041980891960ae
kieranhirsh <kieranhirsh@gmail.com>
2024-03-12 13:05:06 +0000

    C - Structures, typedef. Task 3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  * Description: this struct is for the "C - Structures, typedef" project
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
