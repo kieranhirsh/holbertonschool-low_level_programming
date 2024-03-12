f7d2aafc74f5207074970539af0e6b224ac1239c
kieranhirsh <kieranhirsh@gmail.com>
2024-03-12 02:09:41 +0000

    C - malloc, free. Task 3. If you even dream of beating me you'd better wake up and apologize

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a 2D array of ints, and initializes it
+ * @width: the width of the grid
+ * @height: the height of the grid
+ *
+ * Return: NULL, if failure
+ *         a pointer to the array otherwise
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int ii;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+
+	for (ii = 0 ; ii < height ; ii++)
+	{
+		array[ii] = malloc(sizeof(int) * width);
+		if (array[ii] == NULL)
+			return (NULL);
+	}
+
+	return (array);
+}
+
