a43b3311ce3c46820c839969952d4d62bab6e557
kieranhirsh <kieranhirsh@gmail.com>
2024-03-12 01:51:08 +0000

    C - malloc, free. Task 2. He who is not courageous enough to take risks will accomplish nothing in life. fixing NULL case

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,6 +15,11 @@ char *str_concat(char *s1, char *s2)
 	int size1 = 0, size2 = 0;
 	int ii, jj;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[size1] != '\0')
 		size1 += 1;
 
