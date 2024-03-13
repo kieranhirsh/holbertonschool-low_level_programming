4f892167bf91b326242b13a6a6a4a065e2a87857
kieranhirsh <kieranhirsh@gmail.com>
2024-03-13 05:55:15 +0000

    C - malloc, free. Task 6. I will show you how great I am

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: the string
+ *
+ * Return: NULL, if failure
+ *         a pointer to the array of strings otherwise
+ *
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int nwords = 0;
+	int wordstart, wordend;
+	int ii = 0, jj = 0;
+	int kk;
+
+	if ((*str == '\0') || (str == NULL))
+		return (NULL);
+
+	while (str[ii] != '\0')
+	{
+		if (((str[ii + 1] == ' ') || (str[ii + 1] == '\0')) && (str[ii] != ' '))
+			nwords += 1;
+		ii += 1;
+	}
+
+	words = malloc(sizeof(char *) * (nwords + 1));
+	if (words == NULL)
+		return (NULL);
+
+	for (ii = 0 ; ii < nwords ; ii++)
+	{
+		while (str[jj] == ' ')
+			jj += 1;
+		wordstart = jj;
+		while (str[jj] != ' ')
+			jj += 1;
+		wordend = jj;
+
+		words[ii] = malloc(sizeof(char) * (wordend - wordstart + 1));
+		if (words[ii] == NULL)
+		{
+			for (kk = 0 ; kk < ii; kk++)
+				free(words[kk]);
+			free(words);
+			return (NULL);
+		}
+
+		for (kk = wordstart ; kk < wordend ; kk++)
+			words[ii][kk - wordstart] = str[kk];
+		words[ii][wordend - wordstart] = '\0';
+	}
+	words[nwords] = '\0';
+
+	return (words);
+}
+
