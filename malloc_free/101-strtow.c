e577c19b0b7fa348bd09560c93b4326634336a36
kieranhirsh <kieranhirsh@gmail.com>
2024-03-13 06:52:48 +0000

    C - malloc, free. Task 6. I will show you how great I am. making the word length calculator its own separate function for readability

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -23,6 +23,33 @@ int wordcount(char *str)
 }
 
 /**
+ * wordlength - finds the length of a given word in a string
+ * @str: the string
+ * @startchar: the char to start at
+ * @wordstart: the first char in the word
+ * @wordend: the last char in the word
+ *
+ * Return: the length of the word
+ *
+ */
+int wordlength(char *str, int *startchar, int *wordstart, int *wordend)
+{
+	int length = 0;
+
+	while (str[*startchar] == ' ')
+		*startchar += 1;
+	*wordstart = *startchar;
+	while ((str[*startchar] != ' ') && (str[*startchar] != '\0'))
+	{
+		length += 1;
+		*startchar += 1;
+	}
+	*wordend = *startchar;
+
+	return (length);
+}
+
+/**
  * strtow - splits a string into words
  * @str: the string
  *
@@ -33,8 +60,8 @@ int wordcount(char *str)
 char **strtow(char *str)
 {
 	char **words;
-	int nwords = 0;
-	int wordstart, wordend;
+	int nwords = 0, wordstart = 0, wordend = 0;
+	int length;
 	int ii = 0, jj = 0;
 	int kk;
 
@@ -51,17 +78,12 @@ char **strtow(char *str)
 
 	for (ii = 0 ; ii < nwords ; ii++)
 	{
-		while (str[jj] == ' ')
-			jj += 1;
-		wordstart = jj;
-		while (str[jj] != ' ')
-			jj += 1;
-		wordend = jj;
+		length = wordlength(str, &jj, &wordstart, &wordend);
 
-		words[ii] = malloc(sizeof(char) * (wordend - wordstart));
+		words[ii] = malloc(sizeof(char) * (length + 1));
 		if (words[ii] == NULL)
 		{
-			for (kk = 0 ; kk < ii; kk++)
+			for (kk = 0 ; kk < ii ; kk++)
 				free(words[kk]);
 			free(words);
 			return (NULL);
