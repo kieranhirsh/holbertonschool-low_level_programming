efc06205f83034665d1cc4093a7e9041b95d4751
kieranhirsh <kieranhirsh@gmail.com>
2024-03-14 05:12:41 +0000

    C - More malloc, free. Task 5. We must accept finite disappointment, but never lose infinite hope. Added a function to calculate string length

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 /**
+ * _strlen - finds the length of a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int ii = 0;
+
+	while (str[ii] != '\0')
+		ii += 1;
+
+	return (ii);
+}
+
+/**
  * main - check the code
  * @argc: the number of command line arguments passed to the program
  * @argv: the command line arguments passed to the program (unused)
@@ -9,13 +25,17 @@
  */
 int main(int argc, char **argv)
 {
+	int len1, len2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (98);
 
 	}
-	printf("%i\n", argc);
+
+	len1 = _strlen(argv[1]);
+	len2 = _strlen(argv[2]);
 
 	return (0);
 }
