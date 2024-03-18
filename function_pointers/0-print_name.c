45b5713ba9b6410487f52f82763635ef774c9e63
kieranhirsh <kieranhirsh@gmail.com>
2024-03-18 03:06:49 +0000

    C - Function pointers. 0. What's my name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name
+ * @name: the name to be printed
+ * @f: the function that does the printing
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
