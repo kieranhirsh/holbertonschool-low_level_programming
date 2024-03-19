3ebeb8885914b82b2028da30aa26045af0a03da3
kieranhirsh <kieranhirsh@gmail.com>
2024-03-19 03:26:56 +0000

    C - Variadic functions. Task 0. Beauty is variable, ugliness is constant. making betty happy

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -4,6 +4,8 @@
  * sum_them_all - sums a bunch of numbers
  * @n: the number of numbers to be summed
  *
+ * Return: 0 if the number of arguments is 0
+ *         the result of the sum otherwise
  */
 int sum_them_all(const unsigned int n, ...)
 {
