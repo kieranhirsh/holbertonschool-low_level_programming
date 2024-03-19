39963f7c9406e256fcbb06ade2b0c2c86a7d0524
kieranhirsh <kieranhirsh@gmail.com>
2024-03-19 03:53:37 +0000

    C - Variadic functions. Task 1. To be is to be the value of a variable

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints a bunch of numbers
+ * @n: the numbers to be printed
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int number;
+	unsigned int ii;
+	va_list args;
+
+	va_start(args, n);
+
+	for (ii = 0 ; ii < n ; ii++)
+	{
+		number = va_arg(args, int);
+		printf("%i", number);
+		if ((separator != NULL) && (ii != (n - 1)))
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
+
