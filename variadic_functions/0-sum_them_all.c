49b60086dceb9ec6840fdb2c477edcfbd465222e
kieranhirsh <kieranhirsh@gmail.com>
2024-03-19 03:25:53 +0000

    C - Variadic functions. Task 0. Beauty is variable, ugliness is constant

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums a bunch of numbers
+ * @n: the number of numbers to be summed
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int number;
+	int result = 0;
+	unsigned int ii;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (ii = 0 ; ii < n ; ii++)
+	{
+		number = va_arg(args, int);
+		result += number;
+	}
+
+	va_end(args);
+
+	return (result);
+}
+
