22c8b79ee59040a500cd1bfaf6edba1bd6b37195
kieranhirsh <kieranhirsh@gmail.com>
2024-03-19 03:58:11 +0000

    C - Variadic functions. Task 2. One woman's constant is another woman's variable

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints a bunch of strings
+ * @separator: the string to be printed between the numbers
+ * @n: the numbers to be printed
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *string;
+	unsigned int ii;
+	va_list args;
+
+	va_start(args, n);
+
+	for (ii = 0 ; ii < n ; ii++)
+	{
+		string = va_arg(args, char *);
+		printf("%s", string);
+		if ((separator != NULL) && (ii != (n - 1)))
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
+
