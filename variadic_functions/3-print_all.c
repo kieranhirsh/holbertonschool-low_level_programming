b29c44c658a8fbbe9d3cf0f9cce22d0e54b83c41
kieranhirsh <kieranhirsh@gmail.com>
2024-03-20 02:13:57 +0000

    C - Variadic functions. Task 3. To be is a to be the value of a variable

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,122 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_nothing - does nothing
+ * @args: irrelevant
+ *
+ */
+void print_nothing(va_list *args, int *logical)
+{
+	(void) args;
+	(void) logical;
+}
+
+/**
+ * print_char - prints a char
+ * @args: the char to be printed
+ *
+ */
+void print_char(va_list *args, int *logical)
+{
+	char string;
+
+	string = va_arg(*args, int);
+
+	printf("%c", string);
+
+	*logical = 1;
+}
+
+/**
+ * print_string - prints a string
+ * @args: the string to be printed
+ *
+ */
+void print_string(va_list *args, int *logical)
+{
+	char *string;
+
+	string = va_arg(*args, char *);
+
+	if (string == NULL)
+		string = "(nil)";
+	printf("%s", string);
+
+	*logical = 1;
+}
+
+/**
+ * print_int - prints an int
+ * @args: the int to be printed
+ *
+ */
+void print_int(va_list *args, int *logical)
+{
+	int number;
+
+	number = va_arg(*args, int);
+
+	printf("%i", number);
+
+	*logical = 1;
+}
+
+/**
+ * print_float - prints a float
+ * @args: the float to be printed
+ *
+ */
+void print_float(va_list *args, int *logical)
+{
+	double number;
+
+	number = va_arg(*args, double);
+
+	printf("%f", number);
+
+	*logical = 1;
+}
+
+/**
+ * print_all - prints anything
+ * @format: the format of what is to be printed
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	operation op[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', print_nothing}
+	};
+	int has_printed = 0;
+	int ii = 0;
+	int jj;
+	va_list args;
+
+	va_start(args, format);
+
+	while (format[ii] != '\0')
+	{
+		jj = 0;
+		while ((format[ii] != op[jj].type) && (op[jj].type != '\0'))
+		{
+			jj += 1;
+		}
+
+		if (has_printed && (op[jj].type != '\0'))
+			printf(", ");
+		op[jj].func(&args, &has_printed);
+
+		ii += 1;
+	}
+
+	va_end(args);
+
+	printf("\n");
+}
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+#include <stdarg.h>
+
+/**
+ * struct operations - ties a data type to a corresponding function
+ * @tpye: the data type
+ * @func: the function
+ *
+ * Description: this struct stores a char with the corresponding function
+ *              that prints the data type specified with the char
+ */
+typedef struct operations
+{
+	char type;
+	void (*func)(va_list *args, int *logical);
+} operation;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
