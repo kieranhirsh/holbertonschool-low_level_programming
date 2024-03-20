555b1725d8f51bc807043a9c291ed14b25fdcbcb
kieranhirsh <kieranhirsh@gmail.com>
2024-03-20 03:04:05 +0000

    C - Function pointers. Task 4. Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: the number of command line arguments passed to the program (unused)
+ * @argv: the command line arguments passed to the program
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int bytes;
+	int ii;
+	int (*address)(int,char **);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	address = &main;
+
+	for (ii = 0 ; ii < bytes ; ii++)
+	{
+		printf("%.2x ", ((unsigned char *)address)[ii]);
+	}
+	printf("\n");
+
+	return (0);
+}
