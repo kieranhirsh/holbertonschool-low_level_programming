e7dc133b53ca81c996aaac6e2a79b9c0f02bed80
kieranhirsh <kieranhirsh@gmail.com>
2024-04-04 23:24:41 +0000

    C - Singly linked lists. Task 5. The Hare and the Tortoise. fixing compile errors

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * before_main - prints a message BEFORE MAIN() IS EXCECUTED
  *
  */
-void before_main() __attribute__((constructor))
+void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
