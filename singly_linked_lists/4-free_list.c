02ed350fab8bfdc645db8484536a5a64b9172790
kieranhirsh <kieranhirsh@gmail.com>
2024-04-04 07:31:38 +0000

    C - Singly linked lists. Task 4. Free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: the list
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	if (temp != NULL)
+	{
+		while (head != NULL)
+		{
+			temp = head;
+			free((*head).str);
+			free((*head).next);
+			free(head);
+			head = (*temp).next;
+		}
+	}
+}
+
