ee80b5054061ce4c903b321dc631faf671695ee9
kieranhirsh <kieranhirsh@gmail.com>
2024-04-05 05:34:07 +0000

    C - Doubly linked lists. Task 7. Insert at index. getting fancy with linkages

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -39,10 +39,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	(*new).n = n;
 	(*new).next = temp;
 	(*new).prev = (*temp).prev;
-
-	(*temp).prev = new;
-	temp = (*new).prev;
-	(*temp).next = new;
+	(*(*new).next).prev = new;
+	(*(*new).prev).next = new;
 
 	return (new);
 }
