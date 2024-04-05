32c0dc4bf721b0b140dce2008c4dee4ffc35cbba
kieranhirsh <kieranhirsh@gmail.com>
2024-04-05 01:17:05 +0000

    C - Doubly linked lists. Task 2. Add node. Should now correctly link backwards

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,6 +23,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	(*new).next = *head;
 	(*new).prev = NULL;
 
+	if (*head != NULL)
+		(**head).prev = new;
+
 	*head = new;
 
 	return (new);
