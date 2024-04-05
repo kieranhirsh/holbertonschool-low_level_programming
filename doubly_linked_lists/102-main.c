33851f7832995a56c33d8c913a9993c4f90adcb3
kieranhirsh <kieranhirsh@gmail.com>
2024-04-05 06:15:34 +0000

    C - Doubly linked lists. Task 10. Palindromes. actually calculating the desired number

--- doubly_linked_lists/102-main.c
+++ doubly_linked_lists/102-main.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
@@ -11,12 +9,46 @@
 int main(void)
 {
 	FILE *file;
+	int palindrome, product, remainder;
+	int ii, jj, kk;
 
-	file = fopen("102-result", "w");
-	fprintf(file, "906609");
-	fclose(file);
+	for (ii = 9 ; ii >= 0 ; ii--)
+	{
+		for (jj = 9 ; jj >= 0 ; jj--)
+		{
+			for (kk = 9 ; kk >= 0 ; kk--)
+			{
+				palindrome = 0;
 
-	return (0);
+				palindrome += ii * 1;
+				palindrome += jj * 10;
+				palindrome += kk * 100;
+				palindrome += kk * 1000;
+				palindrome += jj * 10000;
+				palindrome += ii * 100000;
+
+				for (product = 100 ; product <= 999 ; product++)
+				{
+					if ((palindrome % product) == 0)
+					{
+						remainder = palindrome / product;
+
+						if (((remainder / 1000) == 0) && ((remainder / 100) > 0))
+						{
+							file = fopen("102-result", "w");
+							fprintf(file, "%i", palindrome);
+							fclose(file);
+
+							return (0);
+
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return (-1);
 
 }
 
