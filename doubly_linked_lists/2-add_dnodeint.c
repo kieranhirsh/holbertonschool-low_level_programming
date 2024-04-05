1a2eb94f5954ca54fe781d1d2228c22634395e4d
kieranhirsh <kieranhirsh@gmail.com>
2024-04-05 01:13:09 +0000

    C - Doubly linked lists. Task 2. Add node. Currently acts only as a singly linked list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a node to the start of a list
+ * @head: a pointer to the start of the list
+ * @n: the int to add to the list
+ *
+ * Return: a pointer to the new head of the list
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	(*new).n = n;
+	(*new).next = *head;
+	(*new).prev = NULL;
+
+	*head = new;
+
+	return (new);
+}
+
