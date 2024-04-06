6bfaddb7087b2a50e97081f83caf24b088a21c8a
kieranhirsh <kieranhirsh@gmail.com>
2024-04-06 08:15:57 +0000

    C - Bit manipulation. Task 0. 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: a string containing 0's and 1's
+ *
+ * Return: 0, if b is NULL
+ *         0, if there is a char in b that is not 0 or 1
+ *         the unsigned int, otherwise
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int uint = 0, len = 0;
+	unsigned int power = 1;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[len] != '\0')
+	{
+		if ((b[len] != '0') && (b[len] != '1'))
+			return (0);
+
+		len += 1;
+	}
+
+	do {
+		len -= 1;
+		uint += (b[len] - 48) * power;
+		power *= 2;
+	}
+	while (len > 0);
+
+	return (uint);
+}
+
---
+++ bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef BIT_MANIP
+#define BIT_MANIP
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
