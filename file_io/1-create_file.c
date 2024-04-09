eb94ec4fc328e52942b484e07816f547235046b9
kieranhirsh <kieranhirsh@gmail.com>
2024-04-09 14:45:47 +0000

    C - File I/O. Task 1. Under the snow

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - creates a text file
+ * @filename: the name of the file to create
+ * @text_content: the content of the file to create
+ *
+ * Return: 1, on success
+ *         -1, on failure
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	int len = 0;
+	int byteswritten;
+
+	while (text_content[len] != '\0')
+		len += 1;
+
+	byteswritten = write(fd, text_content, len);
+
+	if ((filename == NULL) || (fd == -1) | (byteswritten != len))
+		return (-1);
+
+	return (1);
+}
+
