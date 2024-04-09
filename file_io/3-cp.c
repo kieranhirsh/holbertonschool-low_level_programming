70c5e18ca9a7d5707f87418b2bb47c1ae0e4777d
kieranhirsh <kieranhirsh@gmail.com>
2024-04-09 16:18:34 +0000

    C - File I/O. Task 3. cp. fixing compile errors

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 	char *file_to = argv[2];
 	int fdfrom = open(file_from, O_RDONLY);
 	int fdto = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	int bytesread = 1024, byteswritten;
+	int bytesread, byteswritten;
 
 	if (argc != 3)
 	{
@@ -53,21 +53,20 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		return (99);
 	}
-	while (bytesread != 0)
-	{
+	do {
 		bytesread = read(fdfrom, buffer, bytesread);
 		if (bytesread == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			return (98);
 		}
-		byteswritten = write(fdto, buffer, bytesread)
+		byteswritten = write(fdto, buffer, bytesread);
 		if (byteswritten == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			return (99);
 		}
-	}
+	} while (bytesread != 0);
 	close_file(fdfrom);
 	close_file(fdto);
 	return (0);
