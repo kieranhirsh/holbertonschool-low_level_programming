223c2a9af6c0e160ea14fcbef5977b99d151ab7b
kieranhirsh <kieranhirsh@gmail.com>
2024-04-09 14:59:16 +0000

    C - File I/O. Task 2. Speak gently, she can hear

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: the name of the file to append text to
+ * @text_content: the content to append
+ *
+ * Return: 1, on success
+ *         -1, on failure
+ *
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 0600);
+	int len = 0;
+	int byteswritten;
+
+	if ((filename == NULL) || (fd == -1))
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	while (text_content[len] != '\0')
+		len += 1;
+
+	byteswritten = write(fd, text_content, len);
+	close(fd);
+
+	if (byteswritten != len)
+		return (-1);
+
+	return (1);
+}
+
