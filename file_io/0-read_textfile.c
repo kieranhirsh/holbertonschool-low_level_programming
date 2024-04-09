2e795bd37f53be1939dec2b233c0f0279b425c18
kieranhirsh <kieranhirsh@gmail.com>
2024-04-09 14:17:48 +0000

    C - File I/O. Task 0. Tread lightly, she is near

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: the file
+ * @letters: then number of characters to print
+ *
+ * Return: the number of characters printed
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char* buffer = malloc(sizeof(char) * letters);
+	int fd = open(filename, O_RDONLY);
+	int bytesread = read(fd, buffer, letters);
+
+	if ((filename == NULL) || (fd == -1) || (buffer == NULL))
+		return (0);
+
+	write(1, buffer, bytesread);
+
+	return (write(STDOUT_FILENO, buffer, bytesread));
+}
+
